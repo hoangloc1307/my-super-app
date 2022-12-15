ec8bbac955acd35218f94f4b26084066448dc74a
Tran Nguyen Hoang Loc <hoangloc1307@gmail.com>
2022-12-15 16:16:16 +0700

    Add compress image

--- src/pages/CompressImage/CompressImage.jsx
+++ src/pages/CompressImage/CompressImage.jsx
@@ -1,56 +1,126 @@
-import { Button, Stack, TextField } from '@mui/joy';
-import { FileUpload, PhotoSizeSelectLarge } from '@mui/icons-material';
+import { Button, Stack } from '@mui/joy';
+import { Delete, FileUpload, FileDownload, PhotoSizeSelectLarge } from '@mui/icons-material';
 import { useState } from 'react';
 import axios from 'axios';
+import { IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+
+import { numberWithSizeUnit } from '~/utils/stringFormat';
+
+const litmitImage = 20;
 
 export default function CompressImage() {
-    const [selectedFile, setSelectedFile] = useState({});
+    const [selectedFiles, setSelectedFiles] = useState([]);
 
     const handleUpload = (event) => {
-        setSelectedFile(event.target.files);
+        const uploadingFiles = [...event.target.files];
+        const uploadedFiles = [...selectedFiles];
+        while (uploadedFiles.length < litmitImage && uploadingFiles.length > 0) {
+            const uploadingFile = uploadingFiles.shift();
+            let isExists = false;
+            for (const file of uploadedFiles) {
+                if (file.name === uploadingFile.name && file.size === uploadingFile.size) {
+                    isExists = true;
+                    break;
+                }
+            }
+            if (!isExists) {
+                uploadedFiles.push(uploadingFile);
+            }
+        }
+        if (uploadedFiles.length !== selectedFiles.length) {
+            setSelectedFiles(uploadedFiles);
+        }
     };
 
-    const handleOptimize = () => {
-        const data = JSON.stringify({
-            source: {
-                url: 'https://i.imgur.com/zDad6bb.jpg'
-            }
-        });
+    const handleRemoveImage = (file) => {
+        const index = selectedFiles.findIndex((item) => item.name === file.name && item.size === file.size);
 
-        const config = {
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            withCredentials: false,
-            auth: {
-                username: 'api',
-                password: process.env.REACT_APP_TINIFY_API_KEY
-            }
-        };
+        const uploadedFiles = [...selectedFiles.slice(0, index), ...selectedFiles.slice(index + 1)];
 
-        axios
-            .post('https://api.tinify.com/shrink', data, config)
-            .then((res) => console.log(res))
-            .catch((error) => console.log(error));
+        setSelectedFiles(uploadedFiles);
+    };
+
+    const handleOptimize = async () => {
+        // const data = JSON.stringify({
+        //     source: {
+        //         url: 'https://i.imgur.com/zDad6bb.jpg'
+        //     }
+        // });
+
+        // const config = {
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     },
+        //     withCredentials: false,
+        //     auth: {
+        //         username: 'api',
+        //         password: process.env.REACT_APP_TINIFY_API_KEY
+        //     }
+        // };
+
+        // axios
+        //     .post('https://api.tinify.com/shrink', data, config)
+        //     .then((res) => console.log(res))
+        //     .catch((error) => console.log(error));
+
+        const file = selectedFiles[0];
+        // const fileReader = new FileReader();
+        // fileReader.onload = () => {
+        //     console.log(fileReader.result);
+        // };
+        // fileReader.readAsArrayBuffer(file);
+        console.log(URL.createObjectURL(file));
     };
 
     return (
         <>
-            <Stack direction={'row'}>
+            <Stack direction={'row'} spacing={2}>
                 <Button component="label" startDecorator={<FileUpload />}>
                     Upload
-                    <input hidden accept="image/*" multiple type="file" onChange={handleUpload} />
+                    <input hidden accept="image/*" multiple type="file" value={[]} onChange={handleUpload} />
                 </Button>
-                <TextField
-                    placeholder="No file choosen..."
-                    value={selectedFile[0]?.name || ''}
-                    sx={{ ml: 1, mr: 3, width: '200px' }}
-                    disabled
-                />
                 <Button startDecorator={<PhotoSizeSelectLarge />} loading={false} onClick={handleOptimize}>
                     Optimize
                 </Button>
             </Stack>
+            {/* List image */}
+            <TableContainer component={Paper} sx={{ mt: 3 }}>
+                <Table stickyHeader size="small">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Name</TableCell>
+                            <TableCell>Size</TableCell>
+                            <TableCell>Status</TableCell>
+                            <TableCell>Action</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {selectedFiles.length > 0 ? (
+                            selectedFiles.map((item) => (
+                                <TableRow key={item.name + item.size}>
+                                    <TableCell>{item.name}</TableCell>
+                                    <TableCell>{numberWithSizeUnit(item.size)}</TableCell>
+                                    <TableCell>Not optimize</TableCell>
+                                    <TableCell>
+                                        <IconButton size="small" color="primary">
+                                            <FileDownload />
+                                        </IconButton>
+                                        <IconButton size="small" color="error" onClick={() => handleRemoveImage(item)}>
+                                            <Delete />
+                                        </IconButton>
+                                    </TableCell>
+                                </TableRow>
+                            ))
+                        ) : (
+                            <TableRow>
+                                <TableCell align="center" colSpan={4}>
+                                    Up to {litmitImage} images, no size limit!
+                                </TableCell>
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </>
     );
 }
--- src/utils/stringFormat.js
+++ src/utils/stringFormat.js
@@ -1,3 +1,12 @@
 export function numberWithCommas(x) {
     return x?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
 }
+
+export function numberWithSizeUnit(x) {
+    if (x >= 1048576) {
+        return (Math.round((x / 1048576 + Number.EPSILON) * 100) / 100)?.toString() + ' MB';
+    } else if (x >= 1024) {
+        return (Math.round((x / 1024 + Number.EPSILON) * 100) / 100)?.toString() + ' KB';
+    }
+    return x;
+}
